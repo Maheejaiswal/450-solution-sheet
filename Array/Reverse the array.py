816fff5c91a965b66ee470b9bb4eb9a950dd9d24
greeneyedcoder <mahee.jaiswal@cumminscollege.edu.in>
2021-06-22 15:41:35 +0530

    Update Reverse the array.py

--- Array/Reverse the array.py
+++ Array/Reverse the array.py
@@ -1,7 +1,34 @@
-'''
+# approach 1 : slicing
 def reverseList(A)
   return A[::-1]
   
 A=[11,77,88]
 print(reverseList(A))
-'''
+
+# approach 2 :Recursive way
+def reverseList(A, start, end):
+    if start >= end:
+        return
+    A[start], A[end] = A[end], A[start]
+    reverseList(A, start+1, end-1)
+ 
+# Driver function to test above function
+A = [1, 2, 3, 4, 5, 6]
+print(A)
+reverseList(A, 0, 5)
+print("Reversed list is")
+print(A)
+
+#approach 3 : iterative way
+def reverseList(A, start, end):
+    while start < end:
+        A[start], A[end] = A[end], A[start]
+        start += 1
+        end -= 1
+ 
+# Driver function to test above function
+A = [1, 2, 3, 4, 5, 6]
+print(A)
+reverseList(A, 0, 5)
+print("Reversed list is")
+print(A)
