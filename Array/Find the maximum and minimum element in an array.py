0c1c1143a18462f8bb723103b68f21fe06d2c59a
greeneyedcoder <mahee.jaiswal@cumminscollege.edu.in>
2021-06-22 16:31:58 +0530

    Update Find the maximum and minimum element in an array.py

--- Array/Find the maximum and minimum element in an array.py
+++ Array/Find the maximum and minimum element in an array.py
@@ -12,3 +12,25 @@ arr=[3,4,9,2]
 max,min=maxMin(arr)
 print(max)
 print(min)
+---------------------------------------------------------------------------------------
+def maxi(arr):
+    return max(arr)
+
+def mini(arr):
+    return min(arr)
+  
+arr=[1,2,3,4,5,9]
+print(maxi(arr))
+print(mini(arr))
+---------------------------------------------------------------------------------------
+def maximumElement(arr,n):
+    arr.sort()
+    return arr[-1]
+
+def minimumElement(arr,n):
+    arr.sort()
+    return arr[0]
+
+arr=[1,2,3,4,5,9]
+print(maximumElement(arr))
+print(minimumElement(arr))
