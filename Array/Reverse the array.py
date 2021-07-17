7e2df479b650fb96a15430375b890ded648dd3ae
greeneyedcoder <mahee.jaiswal@cumminscollege.edu.in>
2021-07-17 16:35:40 +0530

    Update Reverse the array.py

--- Array/Reverse the array.py
+++ Array/Reverse the array.py
@@ -1,29 +1,39 @@
-# approach 1 : slicing
+# Write a program to reverse an array or string 
+## approach 1 : slicing
+'''
 def reverseList(A)
   return A[::-1]
----------------------------------------------------------------------------------------------------------------
-# approach 2 :Recursive way
+'''
+
+## approach 2 :Recursive way
+'''
 def reverseList(A, start, end):
     if start >= end:
         return
     A[start], A[end] = A[end], A[start]
     reverseList(A, start+1, end-1)
+'''
 
-------------------------------------------------------------------------------------------------------------------
-#approach 3 : iterative way
+## approach 3 : iterative way
+'''
 def reverseList(A, start, end):
     while start < end:
         A[start], A[end] = A[end], A[start]
         start += 1
         end -= 1
-        
--------------------------------------------------------------------------------------------------------------------
-#reverse a string
+ '''       
+
+## reverse a string
+'''
 def reverseWord(s):
     rev=" "
     for i in s:
         rev=i+rev
     return rev
-  -----------------------------------------------------------------------------------------------------------------
-  def reverseWord(s):
+'''
+ 
+  ## string reverse
+'''
+def reverseWord(s):
     return s[::-1]
+'''
